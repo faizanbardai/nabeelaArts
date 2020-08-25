36e660a833b457acaa2ebc428b0c85b1dbce6b3e
FayJu <faizan@unetiq.com>
2020-08-26 00:12:56 +0200

    Updates

---
+++ precache-manifest.65c35e454423af864f5f0458c4d5ffd3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47281717f8884b0ab39f7fc741998ab9",
+    "url": "/nabeelaArts/index.html"
+  },
+  {
+    "revision": "73bb9e19bef2cb404f7d",
+    "url": "/nabeelaArts/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "25e3c0c14a78c93affb1",
+    "url": "/nabeelaArts/static/js/2.6bcfe6a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nabeelaArts/static/js/2.6bcfe6a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73bb9e19bef2cb404f7d",
+    "url": "/nabeelaArts/static/js/main.b8fce80d.chunk.js"
+  },
+  {
+    "revision": "3930e2e481c2290554ae",
+    "url": "/nabeelaArts/static/js/runtime-main.55a6279b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nabeelaArts/static/media/logo.5d5d9eef.svg"
+  }
+]);
