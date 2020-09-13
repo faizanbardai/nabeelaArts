8f27d5f6f5e9442e67d3864958c5e4da8e12ee6d
FayJu <faizan@unetiq.com>
2020-09-14 01:59:56 +0200

    /admin route

--- src/Services/index.js
+++ src/Services/index.js
@@ -1,6 +1,6 @@
 const baseURL = process.env.REACT_APP_BASE_URL;
 export const api_login = async (body) => {
-    return await fetch(baseURL + "/login", {
+    return await fetch(baseURL + "/admin/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -10,7 +10,7 @@ export const api_login = async (body) => {
   };
 
   export const api_refresh_token = async (token) => {
-    return await fetch(baseURL + "/refresh-token", {
+    return await fetch(baseURL + "/admin/refresh-token", {
       headers: {
         "Content-Type": "application/json",
         Authorization: "Bearer " + token,
