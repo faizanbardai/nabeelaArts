a62df3e0aad7490b464776576466fbc23a3a3fa3
FayJu <faizan@unetiq.com>
2020-09-16 20:05:37 +0200

    Add or update art component

---
+++ src/Components/Admin/AddUpdateArt.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AddUpdateArt() {
+  return (
+    <div>
+      Add or Update Art
+    </div>
+  )
+}
--- src/Components/Admin/AdminPanel.jsx
+++ src/Components/Admin/AdminPanel.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import AddUpdateArt from './AddUpdateArt'
 
 export default function AdminPanel() {
     return (
         <div>
             <div>I'm logged in</div>
+            <AddUpdateArt />
         </div>
     )
 }
