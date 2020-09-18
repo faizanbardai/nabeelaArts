4f8497e81cabbd2a337ea1a4b7f5997d77700896
FayJu <faizan@unetiq.com>
2020-09-19 00:51:44 +0200

    Removed menu icon

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,11 +3,9 @@ import { makeStyles } from "@material-ui/core/styles";
 import {
   AppBar,
   Button,
-  IconButton,
   Toolbar,
   Typography,
 } from "@material-ui/core";
-import MenuIcon from "@material-ui/icons/Menu";
 import { withRouter } from "react-router-dom";
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -25,23 +23,37 @@ const Header = (props) => {
   const nextPath = (path) => {
     props.history.push(path);
   };
+  const logout = () => {
+    localStorage.removeItem("token");
+    props.setIsLoggedIn(false);
+  };
   return (
     <AppBar position="static">
       <Toolbar>
-        <IconButton
+        {/* <IconButton
           edge="start"
           className={classes.menuButton}
           color="inherit"
           aria-label="menu"
         >
           <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" className={classes.title}>
+        </IconButton> */}
+        <Typography
+          variant="h6"
+          className={classes.title}
+          onClick={() => nextPath("/")}
+        >
           Nabeela's Art
         </Typography>
-        <Button color="inherit" onClick={() => nextPath("/admin")}>
-          Admin
-        </Button>
+        {props.isLoggedIn ? (
+          <Button color="inherit" onClick={() => logout()}>
+            Logout
+          </Button>
+        ) : (
+          <Button color="inherit" onClick={() => nextPath("/admin")}>
+            Admin
+          </Button>
+        )}
       </Toolbar>
     </AppBar>
   );
